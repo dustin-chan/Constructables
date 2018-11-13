0ed33a907d500037fb971707dbaebfb7ac2e811c
dustin-chan <contact.dustinchan@gmail.com>
2018-11-13 17:49:22 -0500

    Dependencies Fix

--- frontend/reducers/users_reducer.js
+++ frontend/reducers/users_reducer.js
@@ -5,10 +5,10 @@ import { RECEIVE_CURRENT_USER } from '../actions/session_actions';
 const usersReducer = (state = {}, action) => {
   switch(action.type) {
     case RECEIVE_CURRENT_USER:
-      return merge({}, state, {[action.currentUser.id]: action.currentUser})
+      return merge({}, state, {[action.currentUser.id]: action.currentUser});
     default:
-      return state:
+      return state;
   }
-}
+};
 
 export default usersReducer;
