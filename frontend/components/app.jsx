7cdce6ba764f5f39f65138599462bcfdd11330c4
dustin-chan <contact.dustinchan@gmail.com>
2018-11-13 17:58:39 -0500

    Auth?

---
+++ frontend/components/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { Route, Redirect, Switch, Link, HashRouter } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <div>
+    </div>
+  );
+};
+
+export default App;
